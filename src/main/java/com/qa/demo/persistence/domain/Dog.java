41ae0ea4a4f337e3ec3a4767aa6b9cbef99cc078
a799197 <a799197@host.docker.internal>
2020-11-09 14:16:30 +0000

    added dog entity and other files

---
+++ src/main/java/com/qa/demo/persistence/domain/Dog.java
@@ -0,0 +1,62 @@
+package com.qa.demo.persistence.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity //define table
+public class Dog {
+	
+	@Id //define the id variable
+	@GeneratedValue //id is auto incremented
+	private Long id; //large number type
+	private String breed;
+	private int age;
+	private String colour;
+	private String size;
+	
+	public Dog() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getBreed() {
+		return breed;
+	}
+
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getColour() {
+		return colour;
+	}
+
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+}
